ba2a3f83d201dafa1e309238f4b30ebcdd340ba3
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-05-30 16:27:32 -0300

    feat: add listing of plrs

--- back-end/routes.py
+++ back-end/routes.py
@@ -5,9 +5,6 @@ from userList import UserList
 app = FastAPI()
 
 @app.get("/user/ {username} / {skill} ")
-async def get_user(username: str, skill: str):
+async def add_user(username: str, skill: str):
     return UserList.add_user(User(username, skill))
 
-@app.post("/user/ {username} ")
-async def get_user(username: str):
-    return UserList.get_userbyUsername(username)
---
+++ front-end/router.js
@@ -0,0 +1,32 @@
+const localhost = 'http://localhost:3000';
+
+
+function postRequest(data){
+    fetch(`${localhost}/api`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function getRequest(){
+    fetch(`${localhost}/api`)
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+export { postRequest, getRequest };
